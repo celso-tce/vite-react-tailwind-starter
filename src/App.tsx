23ce05ea51a13b7bb10341c865a49b28938bbb06
Celso TCE <celso.filgueiras@tce.ap.gov.br>
2024-03-04 09:42:44 -0300

    hello

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-w-screen min-h-screen bg-slate-800 text-white flex flex-col">
+      <div className="grow flex justify-center items-center">
+        Oi
+      </div>
+    </div>
+  )
+}
